20c7922e82632e0555e1900cdb707a6b11023477
Furnich <lukmanov.nikita06@mail.ru>
2025-01-16 02:04:58 +0300

    улучшение кода в части чата

--- anonymous_chat/chats/dao.py
+++ anonymous_chat/chats/dao.py
@@ -1,17 +1,23 @@
-from fastapi import Depends
+import logging
+from fastapi import Depends, WebSocketDisconnect
+from fastapi.websockets import WebSocketState
 from sqlalchemy import insert
+from anonymous_chat.chats.crypto import encrypt
 from anonymous_chat.chats.model import Chat
 from anonymous_chat.dao.base import BaseDAO
 from anonymous_chat.database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 from random import randint
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 
 class ChatDAO(BaseDAO):
     model = Chat
 
     async def create_chat_for_users(user1_id: int, user2_id: int, db: AsyncSession = Depends(get_db)) -> str:
-        chat_id = randint(10000, 9999999999)
+        chat_id = randint(10000, 2147483647)
         data = {
             'id': chat_id,
             'user1_id': user1_id,
@@ -27,3 +33,26 @@ class ChatDAO(BaseDAO):
         if not chat:
             return {"msg": "Чат не найден"}
         return chat
+
+    async def handle_user(ws, other_ws, user_id, key):
+        try:
+            logger.info(f"Начало обработки пользователя {user_id}.")
+            while True:
+                if ws.client_state == WebSocketState.CONNECTED:
+                    data = await ws.receive_text()
+                    logger.info(f"Получено сообщение от пользователя {user_id}: {data}")
+                    encrypted_message = encrypt(data, key)
+                    if other_ws.client_state == WebSocketState.CONNECTED:
+                        await other_ws.send_text(f"Пользователь {user_id}: {encrypted_message}")
+                        logger.info(f"Сообщение отправлено пользователю {other_ws}.")
+                else:
+                    logger.warning(f"Соединение с пользователем {user_id} закрыто, выход из цикла.")
+                    break
+        except WebSocketDisconnect as e:
+            logger.info(f"Пользователь {user_id} отключился. {e}")
+        except Exception as e:
+            logger.error(f"Ошибка при обработке пользователя {user_id}: {e}")
+        finally:
+            if ws.client_state == WebSocketState.CONNECTED:
+                await ws.close()
+                logger.info(f"Соединение с пользователем {user_id} закрыто в блоке finally.")
--- anonymous_chat/chats/router.py
+++ anonymous_chat/chats/router.py
@@ -1,48 +1,57 @@
+import logging
+import asyncio
 from collections import deque
-from fastapi import APIRouter, Query, WebSocket
-from anonymous_chat.chats.crypto import encrypt, generate_key
+from fastapi import APIRouter, Depends, Query, WebSocket
+from fastapi.websockets import WebSocketState
+from anonymous_chat.chats.crypto import generate_key
 from anonymous_chat.chats.dao import ChatDAO
+from anonymous_chat.database import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 router = APIRouter(
     prefix="",
     tags=["Чат"]
 )
 
-waiting_users = deque()  # Очередь пользователей, ожидающих чат
+waiting_users = deque()
 active_connections = {}
 
 
 @router.websocket("/ws/chat")
-async def chat_websocket(websocket: WebSocket, user_id: int = Query(...)):
-    print(user_id)
-    print('0000000000')
+async def chat_websocket(websocket: WebSocket, user_id: int = Query(...), db: AsyncSession = Depends(get_db)):
+
+    logger.info(f"Пользователь {user_id} подключился.")
     await websocket.accept()
-    key = generate_key()  # Генерация ключа один раз при подключении
-    waiting_users.append(websocket)  # Добавляем пользователя в очередь
+    key = generate_key()
+    waiting_users.append(websocket)
     active_connections[websocket] = user_id
-    print('111111111')
+
     if len(waiting_users) >= 2:
         user1_ws = waiting_users.popleft()
         user2_ws = waiting_users.popleft()
-        print("1/5.1/5.1/5")
         user1_id = active_connections[user1_ws]
         user2_id = active_connections[user2_ws]
-        print("2222222222222")
-        chat_id = await ChatDAO.create_chat_for_users(user1_id, user2_id)
-        await user1_ws.send_text(f"You are now in chat {chat_id}")
-        await user2_ws.send_text(f"You are now in chat {chat_id}")
-        print('333333333333333')
-        # Основной цикл для обработки сообщений
-        while True:
-            try:
-                data = await user1_ws.receive_text()
-                encrypted_message = encrypt(data, key)
-                await user2_ws.send_text(f"Пользователь 1: {encrypted_message}")
 
-                data = await user2_ws.receive_text()
-                encrypted_message = encrypt(data, key)
-                await user1_ws.send_text(f"Пользователь 2: {encrypted_message}")
-
-            except Exception as e:
-                print("Ошибка", e)
-                break
+        logger.info(f"Создание чата для пользователей {user1_id} и {user2_id}.")
+        try:
+            chat_id = await ChatDAO.create_chat_for_users(user1_id, user2_id, db)
+            logger.info(f"Чат создан с ID {chat_id}. Уведомление пользователей.")
+            if user1_ws.client_state == WebSocketState.CONNECTED:
+                if user2_ws.client_state == WebSocketState.CONNECTED:
+                    await asyncio.gather(
+                        await ChatDAO.handle_user(user1_ws, user2_ws, user1_id, key),
+                        await ChatDAO.handle_user(user2_ws, user1_ws, user2_id, key)
+                    )
+        except Exception as e:
+            logger.error(f"Ошибка при создании чата: {e}")
+        finally:
+            if user1_ws.client_state == WebSocketState.CONNECTED:
+                await user1_ws.close()
+                logger.info(f"Соединение с пользователем {user1_id} закрыто.")
+            if user2_ws.client_state == WebSocketState.CONNECTED:
+                await user2_ws.close()
+                logger.info(f"Соединение с пользователем {user2_id} закрыто.")
